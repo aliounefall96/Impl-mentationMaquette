d0eddb0e31aedd7cd4ffa69ecda8207241a36a9e
Baye Cheikh Fall <52937534+baye94@users.noreply.github.com>
2020-02-14 09:42:19 +0000

    Add files via upload

---
+++ projetMaquette/View/maquette.view.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/bootstrap.min.css">
+    <title>Implémentation de Maquette</title>
+</head>
+<body>
+    <div id="ProjetGlobal">
+        <section id="NavBar">
+            <div class="container-fluid" style="margin-top:-16px;">
+                <div class="row">
+                    <div class="col-6  ">
+                           <hr id="hrHaut">
+                    </div>
+                    <div class="col-6 ">
+                           <hr id="hrBas">
+                    </div>
+                </div>
+            </div>
+          <nav class="navbar navbar-expand-lg   fixed-top " >
+				  <a class="navbar-brand " href="#"><img src="images/logo.png" height="100" width="100" alt=""></a>
+				  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" style="background: #f14950;">
+				    <span class="navbar-toggler-icon"></span>
+				  </button>
+                 <div class="collapse navbar-collapse ml-5 " id="navbarSupportedContent">
+				    <ul class="navbar-nav mr-auto " >
+				      <li class="nav-item ">
+				        <a class="nav-link" href="#accueil2">Accueil</a>
+				      </li>
+				      <li class="nav-item">
+				        <a class="nav-link" href="#Preferences">Prestation</a>
+				      </li>
+				      <li class="nav-item">
+				        <a class="nav-link" href="#References">References</a>
+                      </li>
+                      <li class="nav-item">
+				        <a class="nav-link" href="#contact">Contact</a>
+				      </li>
+				    </ul>
+				  </div>		
+           </nav>
+           <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                           <hr id="hr">
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section  >
+            <div class="container-fluid" style="border: 1px solid black;" id="accueil2">
+              <div class="row">
+                <div class="flex  col-12 col-lg-12">
+                <img src="Images/accueil.jpg"  style="width:100%;" alt=""  >
+                </div>
+              </div>
+           
+            </div>
+        </section>
+        <section id="Preferences">
+              <div class="container-fluid pt-5 pb-5 p-5 offset 5 ">
+                 <div class="row">
+                   <div class="col-12 col-lg-12 text-center">
+                             <hr>
+                                <p><u>Preferences</u></p>
+                   </div>
+                 </div>
+                <div class="row">
+                  <div class="col-12 col-lg-3">
+                    <img src="Images/nibble-3298719_1280.jpg"  height="300" width="300" alt="">
+                    <p style="color:black;">
+                    On ne va pas tourner autour du <br>
+                    pot :la qualité chez Nutella, c’est <br>
+                    notre choix. Dans notre nouvelle <br>
+                    campagne, nous vous invitons à <br>
+                    découvrir les coulisses de nutella <br>
+
+                    </p>
+                  </div>
+                  <div class="col-12 col-lg-3">
+                    <img src="Images/Nutella.jpg" height="300" width="300" alt="">
+                    <p>
+                    On ne va pas tourner autour du <br>
+                    pot :la qualité chez Nutella, c’est <br>
+                    notre choix. Dans notre nouvelle <br>
+                    campagne, nous vous invitons à <br>
+                    découvrir les coulisses de nutella <br>
+
+                    </p>
+                  </div>
+                  <div class="col-12 col-lg-3">
+                    <img src="Images/nutella2.jpg" height="300" width="300" alt="">
+                    <p>
+                    On ne va pas tourner autour du <br>
+                    pot :la qualité chez Nutella, c’est <br>
+                    notre choix. Dans notre nouvelle <br>
+                    campagne, nous vous invitons à <br>
+                    découvrir les coulisses de nutella <br>
+
+                    </p>
+                  </div>
+                  <div class="col-12 col-lg-3">
+                    <img src="Images/nuttella3.jpg" height="300" width="300" alt="">
+                    <p>
+                    On ne va pas tourner autour du <br>
+                    pot :la qualité chez Nutella, c’est <br>
+                    notre choix. Dans notre nouvelle <br>
+                    campagne, nous vous invitons à <br>
+                    découvrir les coulisses de nutella <br>
+
+                    </p>
+                  </div>
+                </div>
+              </div>
+        </section>
+        <section id="References">
+        <div class="container-fluid pt-5 pb-5"  id="accueil2">
+        <div class="row">
+                   <div class="col-12 col-lg-12 text-center">
+                             <hr>
+                                <p><u>References</u></p>
+                   </div>
+                 </div>
+              <div class="row">
+                <div class="flex  col-12 col-lg-12">
+                <img src="Images/Plan.png"  style="width:100%;" alt=""  >
+                </div>
+              </div>
+           
+            </div>
+        </section>
+        <section id="contact">
+            <div class="container-fluid pt-5 pb-5">
+            <div class="row">
+                   <div class="col-12 col-lg-12 text-center">
+                             <hr>
+                                <p><u>Contact</u></p>
+                   </div>
+                 </div>
+            <form action="" method="post" style="margin-left: 0px;">
+              <div class="row">
+             
+                <div class="col-12 col-lg-6">
+                          
+                          <div class="row">
+                          <div class="col-12 col-lg-12">
+                            <input type="text" placeholder="Entrer votre nom" class="input1"><br><br>
+                          </div><br>
+                          <div class="col-12 col-lg-12">
+                            <input type="text" placeholder="Entrer votre prenom" class="input1"><br><br>
+                          </div><br>
+                          <div class="col-12 col-lg-12">
+                            <input type="email" placeholder="Entrer votre email" class="input1"><br><br>
+                          </div><br>
+                          <div class="col-12 col-lg-12">
+                            <input type="text" placeholder="entrer votre ville" class="input1"><br><br>
+                          </div> <br>
+                          <div class="col-12 col-lg-12">
+                            <input type="text" placeholder="entrer votre tel" class="input1"><br><br>
+                          </div>
+                      </div>
+                         
+                     
+                </div>
+               
+               
+                <div class="col-12 col-lg-6 ">
+                <textarea name="" id="" cols="58" rows="15" placeholder="message" class="textarea" ></textarea><br><br>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-12 col-lg-12">
+                   <button type="submit" class="bouton" style="background: red; background:transparent; border:2px solid #f14950; height:1cm;width:2.5cm; ">Envoyer</button>
+                   <button type="reset" class="bouton" style="background: red; background:transparent; border:2px solid #f14950; height:1cm;width:2.5cm;">Annuler</button>
+                </div>
+              </div>
+              </form>
+            </div>
+        </section>
+        <section id="footer">
+       
+			<div class="container-fluid">
+				<div class="row" style="background:#918a8b">
+							<div class="col-md-4" id="logosbas"><img src="images/logo.png" width="100"></div>
+							<div class="col-md-4 mt-5">
+							<strong>	@&Copy 2020</strong>
+							</div>
+							<div class="col-md-4 mt-4" id="reseausociaux">
+								<img src="Images/tiwit.jpg" width="50">
+								<img src="Images/link.png" width="50">
+								<img src="Images/face.png" width="50">
+							</div>
+				</div>
+       
+        </section>
+
+    </div>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ projetMaquette/index.php
@@ -0,0 +1,5 @@
+<?php
+
+
+require 'View/maquette.view.php';
+?>
